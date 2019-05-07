58cc6bc0f05ea3133bb5b4aa286e3abca84410a8
Yevhen V <wik@bdhr.co>
2019-05-07 20:20:54 +0100

    Initial commit

---
+++ lib/learn/analytics/ruby.rb
@@ -0,0 +1,10 @@
+require "learn/analytics/ruby/version"
+
+module Learn
+  module Analytics
+    module Ruby
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/learn/analytics/ruby/version.rb
@@ -0,0 +1,7 @@
+module Learn
+  module Analytics
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
